3cc4f4cf2d012f087dbc924e9855fa5f51d740d9
Nicolas Oga <2112.oga@gmail.com>
2013-06-17 15:35:32 -0300

    arch changes for dell meshuga

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -5,48 +5,48 @@ export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto"
 
 __ruby_version() {
-  # echo `ruby -v | sed -e "s/ (.*//g"`
-  echo ""
+  ruby -v | sed -e "s/ (.*//g"
 }
 
 bash_prompt() {
-  local FILES_INFO="(\$(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g') files, \$(/bin/ls -lah | /bin/grep -m 1 total | /bin/sed 's/total //'))"
+    local NONE="\[\033[0m\]"    # unsets color to term's fg color
 
-  local TITLEBAR="\[\033]0;\u:\w \$(__ruby_version) ${FILES_INFO} \007\]"
+    # regular colors
+    local K="\[\033[0;30m\]"    # black
+    local R="\[\033[0;31m\]"    # red
+    local G="\[\033[0;32m\]"    # green
+    local Y="\[\033[0;33m\]"    # yellow
+    local B="\[\033[0;34m\]"    # blue
+    local M="\[\033[0;35m\]"    # magenta
+    local C="\[\033[0;36m\]"    # cyan
+    local W="\[\033[0;37m\]"    # white
 
-  local NONE="\[\033[0m\]"    # unsets color to term's fg color
+    # emphasized (bolded) colors
+    local EMK="\[\033[1;30m\]"
+    local EMR="\[\033[1;31m\]"
+    local EMG="\[\033[1;32m\]"
+    local EMY="\[\033[1;33m\]"
+    local EMB="\[\033[1;34m\]"
+    local EMM="\[\033[1;35m\]"
+    local EMC="\[\033[1;36m\]"
+    local EMW="\[\033[1;37m\]"
 
-  # regular colors
-  local K="\[\033[0;30m\]"    # black
-  local R="\[\033[0;31m\]"    # red
-  local G="\[\033[0;32m\]"    # green
-  local Y="\[\033[0;33m\]"    # yellow
-  local B="\[\033[0;34m\]"    # blue
-  local M="\[\033[0;35m\]"    # magenta
-  local C="\[\033[0;36m\]"    # cyan
-  local W="\[\033[0;37m\]"    # white
+    # background colors
+    local BGK="\[\033[40m\]"
+    local BGR="\[\033[41m\]"
+    local BGG="\[\033[42m\]"
+    local BGY="\[\033[43m\]"
+    local BGB="\[\033[44m\]"
+    local BGM="\[\033[45m\]"
+    local BGC="\[\033[46m\]"
+    local BGW="\[\033[47m\]"
 
-  # emphasized (bolded) colors
-  local EMK="\[\033[1;30m\]"
-  local EMR="\[\033[1;31m\]"
-  local EMG="\[\033[1;32m\]"
-  local EMY="\[\033[1;33m\]"
-  local EMB="\[\033[1;34m\]"
-  local EMM="\[\033[1;35m\]"
-  local EMC="\[\033[1;36m\]"
-  local EMW="\[\033[1;37m\]"
+    local UC=$EMB               # user's color
+    [ $UID -eq "0" ] && UC=$EMR   # root's color
 
-  # background colors
-  local BGK="\[\033[40m\]"
-  local BGR="\[\033[41m\]"
-  local BGG="\[\033[42m\]"
-  local BGY="\[\033[43m\]"
-  local BGB="\[\033[44m\]"
-  local BGM="\[\033[45m\]"
-  local BGC="\[\033[46m\]"
-  local BGW="\[\033[47m\]"
+    [ $UID -eq "0" ] && USER="\u"
 
-  PS1="$TITLEBAR${B}${BGK}{${USERNAME}}${NONE} ${K}${EMB}\w${Y}${EMW}\$(__git_ps1)\n${NONE}${EMW}▸ ${NONE}"
+    PS1="${UC}${BGK}| ${USER} |${NONE} ${Y}${UC}\w${Y}${UC}${EMW}\$(__git_ps1)\n> ${NONE}"
 }
 
 bash_prompt
